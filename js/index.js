45f36c8d80a7a36a4edbdb82dbb69ce1e611013e
Mercynenoh <mercychepngenoh74@gmail.com>
2022-03-06 14:50:06 +0300

    add js function day

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function day() {
+
+  let input = document.getElementById("dob").value;
+  let day = new Date(input).getUTCDay();
+  var submit = ['Your Day of Birth was : sunday', 'Your Day of Birth was : monday', 'Your Day of Birth was : tuesday', 'Your Day of Birth was : wednesday', 'Your Day of Birth was : thursday', 'Your Day of Birth was : friday', 'Your Day of Birth was : saturday', 'Your Day of Birth was : sunday'];
+  document.getElementById('output').textContent = submit[day];
+
+  
+
+}
