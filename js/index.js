b4771731d358ed8bfbbe03ba8516c9e151a3ab8b
Mercynenoh <mercychepngenoh74@gmail.com>
2022-03-06 15:31:10 +0300

    ad new changes

--- js/index.js
+++ js/index.js
@@ -30,7 +30,7 @@ function date() {
 let input = document.getElementById("dob").value;
 let date = new Date(input).getUTCDay();
 console.log(date)
-var submit = [' You were born on sunday', 'You were born on monday', 'You were born on tuesday', 'You were born on wednesday', 'You were born on thursday', ' You were born on friday', 'You were born on saturday', 'You were born on sunday'];
+var submit = ['You were born on monday', 'You were born on tuesday', 'You were born on wednesday', 'You were born on thursday', ' You were born on friday', 'You were born on saturday', 'You were born on sunday'];
 document.getElementById('output').textContent = submit[date];
 
 let gender = document.getElementById("gender").value;
@@ -40,14 +40,14 @@ let gender = document.getElementById("gender").value;
   let f = submit[date];
 
   if (gender==='male') {
-    var submit = ['Your Akan name is Kwasi', 'Your Akan name is Kwadwo', 'Your Akan name is Kwabena', 'Your Akan name is Kwaku', 'Your Akan name is Yaw', 'Your Akan name is Kofi', 'Your Akan name is Kwame', 'Your Akan name is Kwasi']
+    var submit = ['Your Akan name is Kwadwo', 'Your Akan name is Kwabena', 'Your Akan name is Kwaku', 'Your Akan name is Yaw', 'Your Akan name is Kofi', 'Your Akan name is Kwame', 'Your Akan name is Kwasi']
     document.getElementById('output2').textContent = submit[date];
 
 
     console.log(males[f])
   }
     if (gender === 'female') {
-      var submit = ['Your Akan name is Akosua', 'Your Akan name is Adwoa', 'Your Akan name is Abenaa', 'Your Akan name is Akua', 'Your Akan name is Yaa', 'Your Akan name is Afua', 'Your Akan name is Ama', 'Your Akan name is Akosua']
+      var submit = ['Your Akan name is Adwoa', 'Your Akan name is Abenaa', 'Your Akan name is Akua', 'Your Akan name is Yaa', 'Your Akan name is Afua', 'Your Akan name is Ama', 'Your Akan name is Akosua']
       document.getElementById('output2').textContent = submit[date];
 
       console.log(females[f])
